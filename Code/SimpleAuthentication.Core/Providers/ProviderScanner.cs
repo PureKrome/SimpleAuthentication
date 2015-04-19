c90ebf025e0ed166f76df35d8e4db77a890a3488
Pure Krome <github@world-domination.com.au>
2015-04-20 08:29:49 +1000

    Refactored the ProviderScanner. - The FakeProvider is not in the _main_ default settup. - The constructor that accepts a list of providers is now the full list, not just an -append- list.

--- Code/SimpleAuthentication.Core/Providers/ProviderScanner.cs
+++ Code/SimpleAuthentication.Core/Providers/ProviderScanner.cs
@@ -6,18 +6,21 @@ namespace SimpleAuthentication.Core.Providers
 {
     public class ProviderScanner : IProviderScanner
     {
-        private readonly IList<Type> _additionalTypes;
+        private readonly IList<Type> _explicitProviders;
 
-        public ProviderScanner()
+        public ProviderScanner() : this(DefaultProviders)
         {
         }
 
-        public ProviderScanner(IList<Type> additionalTypes = null)
+        public ProviderScanner(IList<Type> providers)
         {
             // Optional.
-            _additionalTypes = additionalTypes;
+            _explicitProviders = providers;
         }
 
+        /// <summary>
+        /// A common list of providers: Google, Facebook, Twitter, WindowsLive.
+        /// </summary>
         public static IList<Type> DefaultProviders
         {
             get
@@ -27,22 +30,34 @@ namespace SimpleAuthentication.Core.Providers
                     typeof (GoogleProvider),
                     typeof (FacebookProvider),
                     typeof (TwitterProvider),
-                    typeof (WindowsLiveProvider),
-                    typeof (FakeProvider)
+                    typeof (WindowsLiveProvider)
                 };
             }
         }
 
+        /// <summary>
+        /// A common list of providers, including a Fake provider.
+        /// </summary>
+        /// <remarks><b>Note:</b> This includes the <b>FAKE</b> provider (which is great!) but please don't use this in production. ... Just think 'why' I'm warning you, for a sec ... :)</remarks>
+        public static IList<Type> DefaultAndFakeProviders
+        {
+            get
+            {
+                var list = DefaultProviders;
+                list.Add(typeof (FakeProvider));
+
+                return list;
+            }
+        }
+
+        /// <summary>
+        /// A list of providers.
+        /// </summary>
+        /// <remarks>This does <b>NOT</b> do any file system scanning. It's purely what has been provided via the ctor.</remarks>
+        /// <returns></returns>
         public IList<Type> GetDiscoveredProviders()
         {
-            var types = (List<Type>)DefaultProviders;
-            if (_additionalTypes != null &&
-                _additionalTypes.Any())
-            {
-                types.AddRange(_additionalTypes);
-            }
-
-            return types;
+            return _explicitProviders;
         }
     }
 }
